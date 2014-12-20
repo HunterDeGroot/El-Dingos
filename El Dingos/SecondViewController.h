0d925cbb27bb02f3769e932d2db1280080a78724
Hunter DeGroot <huntutinfo@yahoo.com>
2014-12-19 21:29:50 -0600

    Initial Commit

---
+++ El Dingos/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  El Dingos
+//
+//  Created by Hunter DeGroot on 12/19/14.
+//  Copyright (c) 2014 Hunter DeGroot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
